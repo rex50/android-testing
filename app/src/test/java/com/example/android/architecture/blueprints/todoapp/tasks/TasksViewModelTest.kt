c40163e77123c8cf9372e916b39ae3e868669cc5
Pavitra <pareshraut50@gmail.com>
2022-03-15 23:05:00 +0530

    Completed - Testing: ViewModel using Fake Repository

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -3,6 +3,8 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.CoreMatchers.*
 import org.junit.Assert.*
@@ -11,7 +13,6 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
     private lateinit var tasksViewModel: TasksViewModel
@@ -21,7 +22,14 @@ class TasksViewModelTest {
 
     @Before
     fun setupViewModel() {
-        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        val tasksRepository = FakeTestRepository()
+        tasksRepository.addTasks(
+            Task("Title 1", "Description 1"),
+            Task("Title 2", "Description 2", true),
+            Task("Title 3", "Description 3", true)
+        )
+
+        tasksViewModel = TasksViewModel(tasksRepository)
     }
 
     @Test
